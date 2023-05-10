12031059373b3adf5d59c4f6edb47573d676072b
WowCZ <zhenchi713@sjtu.edu.cn>
2023-05-10 13:24:07 +0800

    update openai apis&openai data

--- llm_api/davinci.py
+++ llm_api/davinci.py
@@ -1,5 +1,6 @@
 import random
 import openai
+import time
 from llm_api import LLMAPI, get_logger
 from typing import List
 from pydantic import BaseModel
@@ -15,8 +16,6 @@ with open('assets/openai_keys.txt', 'r') as fr:
 key_id = random.randint(0, len(ks)-1)
 openai_key = ks[key_id]
 
-params = {"temperature": 1.0, "top_p": 1.0, "num_generations": 1, "max_tokens": 512}
-
 class DavinciAPI(LLMAPI):
     def __init__(self, model_name='text-davinci-003', model_path=None):
         super(DavinciAPI, self).__init__(model_name, model_path)
@@ -26,25 +25,36 @@ class DavinciAPI(LLMAPI):
         openai.api_key = openai_key
 
         prompt = item.prompt
-
-        if not prompt:
-            return []
-        
         if type(prompt) is not list:
             prompt = [prompt]
+
+        error = None
+        num_failures = 0
+        while num_failures < 5:
+            try:
+                davinci_replies = []
+                for p in prompt:
+                    completion = openai.Completion.create(model="text-davinci-003",
+                                                            prompt=p,
+                                                            temperature=item.temperature,
+                                                            top_p=item.top_p,
+                                                            max_tokens=item.max_new_tokens,
+                                                            n=item.num_return)
+                    davinci_reply = completion.choices[0].text.strip()
+                    davinci_replies.append(davinci_reply)
+                
+                return davinci_replies
+            except openai.error.RateLimitError as error:
+                logger.warning(error)
+                logger.warning(f"Reach Rate Limit!")
+                num_failures += 1
+                time.sleep(5)
+            except Exception as error:
+                logger.warning(error)
+                num_failures += 1
+                time.sleep(5)
         
-        davinci_replies = []
-        for p in prompt:
-            completion = openai.Completion.create(model="text-davinci-003",
-                                                    prompt=p,
-                                                    temperature=item.temperature,
-                                                    top_p=item.top_p,
-                                                    max_tokens=item.max_new_tokens,
-                                                    n=item.num_return)
-            davinci_reply = completion.choices[0].text.strip()
-            davinci_replies.append(davinci_reply)
-        
-        return davinci_replies
+        raise error
     
 if __name__ == '__main__':
     model_api = DavinciAPI()
